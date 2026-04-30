95474bbcac5275b98121e13f39792503ca04966c
Leo Lo <qet6322076690@gmail.com>
2026-05-01 01:26:01 +0800

    Stop tracking those file.

--- huggleface.py
+++
@@ -1,4 +0,0 @@
-from huggingface_hub import login, upload_folder
-
-
-upload_folder(folder_path="C:/Users/qet63/Documents/Traffic-Violation-Reporting-Automation-TVRA-/dataset", repo_id="leo2783/yolo_detect_Taiwan_Lincense_Plate", repo_type="dataset")
